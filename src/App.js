ba8e1dfeb38e7173651d199b947ac4c94a1f4c28
Vera Kolotyuk <verok@Veras-MacBook-Pro.local>
2025-01-10 13:50:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { v4 as uuid } from 'uuid';
+import { useState, useEffect } from 'react';
 import './App.css';
+import MyList from './MyList';
+import MyMealsAndIngredients from './MyMealsAndIngredients';
 
 function App() {
+  const [mealPlans, setMealPlans] = useState( localStorage.mealPlans ? JSON.parse(localStorage.mealPlans) : []);
+  const [selectedDay, setSelectedDay] = useState(false)
+
+  useEffect( () => {
+    localStorage.setItem("mealPlans", JSON.stringify(mealPlans))
+  },[mealPlans])
+  const addMeal = () =>{
+      const newMeal = {
+        title: "Today is ...",
+        id: uuid(),
+        mealForADay: "",
+        ingredients: ""
+      }
+      setMealPlans([newMeal, ...mealPlans])
+  }
+  const deleteDay = (mealId) => {
+    setMealPlans(mealPlans.filter(({id})=> id !==mealId))
+  }
+  const updateDay = (myUpdatedMeal) => {
+    const updatedMeals = mealPlans.map((mealPlan) => {
+     if(mealPlan.id === myUpdatedMeal.id){
+      return myUpdatedMeal;
+     }
+     return mealPlan;
+    })
+    setMealPlans(updatedMeals)
+  }
+  const getActiveMeal = () => {
+    return mealPlans.find(({id}) => id === selectedDay)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyList  mealPlans={mealPlans}  addMeal={addMeal} deleteDay={deleteDay} selectedDay={selectedDay}
+      setSelectedDay={setSelectedDay}/>
+      <MyMealsAndIngredients selectedDay={getActiveMeal()} updateDay={updateDay}/>
     </div>
   );
 }
---
+++ src/MyMealsAndIngredients.js
@@ -0,0 +1,32 @@
+const MyMealsAndIngredients = ({selectedDay, updateDay}) => {
+    const editMyMeal = (myInput,value ) => {
+        updateDay({
+            ...selectedDay,
+            [myInput] : value
+        })
+    }
+
+    if (!selectedDay) return <p>Plan your week ahead of time!</p>
+
+    return<div className="whole-plan">
+      
+        <input
+        type="text"
+        className="myInput"
+        placeholder="Today is..."
+        id= "title"
+        value={selectedDay.title}
+        onChange={ (e) => editMyMeal("title", e.target.value)}/>
+        <textarea 
+        placeholder="Write your meal plan here"
+        id= "mealForADay"
+        value ={selectedDay.mealForADay}
+        onChange = { (e) => editMyMeal("mealForADay", e.target.value)}/>
+        <textarea
+        placeholder="List of ingredients"/>
+
+  
+    </div>
+    
+}
+export default MyMealsAndIngredients;
